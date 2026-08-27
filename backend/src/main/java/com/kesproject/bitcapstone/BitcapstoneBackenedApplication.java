71da67973bf7a58dbc059db6467ef1b7d88e58a8
Aadit <ap0757258-coder@gmail.com>
2026-08-27 10:24:17 +0530

    Day 5-6 Complete - Spring Boot backend + React frontend with professional login UI

---
+++ backend/src/main/java/com/kesproject/bitcapstone/BitcapstoneBackenedApplication.java
@@ -0,0 +1,13 @@
+package com.kesproject.bitcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcapstoneBackenedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcapstoneBackenedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kesproject/bitcapstone/BitcapstoneBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesproject.bitcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcapstoneBackenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
